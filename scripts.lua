b40a0fdf0ed51298069650e907a42c2cd435e0cf
cframecodes <mrolivesgaming@gmail.com>
2025-01-03 23:15:33 -0800

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -3,6 +3,6 @@ return {
 		name="Car Fucker V1",
 		link_id="407407",
 		link_url="https://work.ink/2jM/carfuckerv1",
-		script_raw="",
+		script_raw="https://raw.githubusercontent.com/cframecodes/cframes_scripts/refs/heads/main/scripts/carfucker.lua",
 	}
 }
